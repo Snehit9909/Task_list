25a9d09c48f64d8320223ab947e2addc4faf10c6
Snehit Kinagi <snehit.n@shellkode.com>
2025-10-29 14:57:25 +0530

    tasks

---
+++ 1.py
@@ -0,0 +1,3 @@
+name=input("Enter your name:")
+greeting= "Hey, " + name + "! Have a good day."
+print(greeting)
---
+++ 2.py
@@ -0,0 +1,10 @@
+num1=float(input("Enter 1st no:"))
+num2=float(input("Enter 2nd no:"))
+
+sum=num1+num2
+product=num1 * num2
+avg=(num1+num2)/2
+
+print("Sum:", sum)
+print("Product:", product)
+print("Average:", avg)
